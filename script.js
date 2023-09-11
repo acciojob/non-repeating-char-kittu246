b2dd51f6bffd0a3c85a18137b33276390800ec89
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-11 14:03:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	let n= str.length;
+
+let i=0;
+let j=i+1;
+	while(i<n && j<n){
+
+    if(str[i]!= str[j]){
+        return(str[i]);
+       
+    }
+    else{
+        while(j<n && str[i]==str[j]){
+            j++;
+        }
+
+        
+
+    }
+
+    i=j;
+    j++;
+}
+
+return "null";
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
